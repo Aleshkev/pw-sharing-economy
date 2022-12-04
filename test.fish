24529e0b12afd02fa5a30437aff0c1117b856e9b
Aleshkev <43296251+Aleshkev@users.noreply.github.com>
2022-12-04 20:47:59 +0100

    save test times + add tokei to the testing script

--- test.fish
+++ test.fish
@@ -4,6 +4,10 @@ set PW_testy ~/PW_testy
 set zip_name ja438241.zip
 set src ~/pw-sharing-economy/src/
 
+if type -q tokei
+    tokei $src
+end
+
 if not test -e $PW_testy
     git clone https://github.com/MIMUW-Inf-2002/PW_testy $PW_testy
 end
